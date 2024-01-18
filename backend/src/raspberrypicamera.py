cade68f943a52bb54bcc7e5588b68244ed91c358
Mark.l <mark_06_20@outlook.com>
2024-01-18 22:14:09 +0100

    raspberrypi_version

--- backend/src/raspberrypicamera.py
+++ backend/src/raspberrypicamera.py
@@ -5,17 +5,6 @@ import threading
 
 camera_routes = Blueprint('camera_routes', __name__)
 thread_lock = threading.Lock()
-@camera_routes.route('/video_feed')
-def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-def background_thread():
-    with app.app_context():
-        global thread_lock
-        with thread_lock:
-            frames = generate_frames()
-            app.frames_iterator = iter(frames)
-
 
 def generate_frames():
     with picamera.PiCamera() as cam:
@@ -27,3 +16,7 @@ def generate_frames():
             cam.capture(stream, 'jpeg', use_video_port=True)
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + stream.getvalue() + b'\r\n')
+
+@camera_routes.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
