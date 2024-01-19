f0c63a1ab057e6e59a28fead90fcb6274796640b
Mark.l <mark_06_20@outlook.com>
2024-01-19 16:41:08 +0100

    socketcamera

---
+++ picamera/combined_sockets.py
@@ -0,0 +1,40 @@
+import io
+import picamera
+import time
+from flask import Flask, render_template, Response
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+def generate_frames():
+    with picamera.PiCamera() as camera:
+        camera.resolution = (300, 250)
+        camera.framerate = 20
+        time.sleep(2)
+        while True:
+            stream = io.BytesIO()
+            camera.capture(stream, format='jpeg', use_video_port=True)
+            yield stream.getvalue()
+            stream.seek(0)
+            stream.truncate()
+
+@app.route('/')
+def index():
+    return render_template('index_combined_sockets.html')
+
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
+@socketio.on('request_frame')
+def handle_request_frame():
+    for frame in generate_frames():
+        socketio.emit('video_frame', {'frame': frame}, namespace='/video_feed')
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=8000, debug=False)
