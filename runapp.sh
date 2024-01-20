7b87d37086beed06af8b5a609653d6478cac82de
Mark Lundage <mark_06_20@outlook.com>
2024-01-20 07:27:50 +0100

    sh script update

--- runapp.sh
+++ runapp.sh
@@ -6,8 +6,14 @@ cd ~/MarkLundager.com
 # Activate the virtual environment
 source ~/venv/bin/activate
 
+
 # Run the Python script
+#gunicorn -k gevent -w 1 -b 0.0.0.0:8000 backend.src.app:app
 #gunicorn -b localhost:8000 backend.src.app:app
-gunicorn -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 3 -b 0.0.0.0:8000 backend.src.app:app
+
+gunicorn -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1 -b 0.0.0.0:8001 backend.src.pi_camera:app &
+gunicorn -b localhost:8000 backend.src.app:app &
+
+#gunicorn -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1 -b 0.0.0.0:8000 backend.src.app:app
 #python3 backend/src/app.py
 
