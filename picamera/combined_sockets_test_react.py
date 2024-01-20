e166304103fdbe3770781b617796e23a1fa95bc5
Mark.l <mark_06_20@outlook.com>
2024-01-20 19:25:22 +0100

    test

--- picamera/combined_sockets_test_react.py
+++ picamera/combined_sockets_test_react.py
@@ -45,4 +45,4 @@ def handle_request_frame():
         print('Frames are already being generated. Ignoring request.')
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=8000, debug=False)
+    socketio.run(app, host='0.0.0.0', port=8001, debug=False)
