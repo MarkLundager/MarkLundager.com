a622f7f37e45e11e960890c5096af5931ba9a4e8
Mark.l <mark_06_20@outlook.com>
2024-01-20 06:04:46 +0100

    changed to run socketio with arg async = gevent

--- backend/src/app.py
+++ backend/src/app.py
@@ -55,5 +55,5 @@ def calculate_time_remaining():
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=8000)
+    socketio.run(app, host='0.0.0.0', port=8000, async_mode='gevent')
     #app.run(host='0.0.0.0', port=8000)
