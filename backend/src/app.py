8c1155a9afc98259eecd6b8b37661acc34f4c657
Mark.l <mark_06_20@outlook.com>
2024-01-20 05:07:20 +0100

    fixed import * from picamera module

--- backend/src/app.py
+++ backend/src/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, jsonify
 from flask_login import login_required, current_user 
 from datetime import datetime
 from .user_routes import user_routes,login_manager
-from .pi_camera import socket_io
+from .pi_camera import socketio
 
 
 #Global variables
@@ -11,7 +11,7 @@ app.config['SECRET_KEY'] = 'c190e4718d190b1e7b956ebbe9339796dc037f4a1dc4d0d5c92b
 app.config['LOGIN_DISABLED'] = False
 login_manager.init_app(app)
 app.register_blueprint(user_routes)
-socket_io.init_app(app)
+socketio.init_app(app)
 
 @app.route('/')
 def index():
