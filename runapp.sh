99f6a937219440932a76a071839d2d57c207a445
Mark Lundage <mark_06_20@outlook.com>
2024-01-21 17:54:17 +0100

    FINALLY WORKS BABY

--- runapp.sh
+++ runapp.sh
@@ -11,8 +11,12 @@ source ~/venv/bin/activate
 #gunicorn -k gevent -w 1 -b 0.0.0.0:8000 backend.src.app:app
 #gunicorn -b localhost:8000 backend.src.app:app
 
-gunicorn -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1 -b 0.0.0.0:8001 backend.src.pi_camera:app &
+python3 backend/src/pi_camera.py &
 gunicorn -b localhost:8000 backend.src.app:app &
+#gunicorn -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1 -b 0.0.0.0:8001 backend.src.pi_camera:app &
+#gunicorn -b localhost:8000 backend.src.app:app &
+#gunicorn -b localhost:8001 backend.src.pi_camera:app &
+
 
 
 
