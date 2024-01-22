28178992f1fa84b16d715d1dd11b1262cd709811
Mark.l <mark_06_20@outlook.com>
2024-01-22 03:15:11 +0100

    test

--- backend/src/pi_camera.py
+++ backend/src/pi_camera.py
@@ -4,42 +4,28 @@ import time
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 
-app = Flask(__name__)
-#socketio = SocketIO(app)
-socketio = SocketIO(app, cors_allowed_origins="https://www.marklundager.com")
+socketapp = Flask(__name__)
+#socketio = SocketIO(socketapp)
+socketio = SocketIO(socketapp, cors_allowed_origins="https://www.marklundager.com")
 connected = 0
 generate_frames_flag = False  # Shared flag to track if frames are being generated
 
-# def generate_frames():
-#     global generate_frames_flag
-#     with picamera.PiCamera() as camera:
-#         camera.resolution = (300), (250)
-#         camera.framerate = 20
-#         time.sleep(2)
-#         while True:
-#             if generate_frames_flag:
-#                 stream = io.BytesIO()
-#                 camera.capture(stream, format='jpeg', use_video_port=True)
-#                 yield stream.getvalue()
-#                 stream.seek(0)
-#                 stream.truncate()
-
 def generate_frames():
     global generate_frames_flag
-    camera = picamera.PiCamera()
-    camera.resolution = (300), (250)
-    camera.framerate = 20
-    time.sleep(2)
-    while True:
-        if generate_frames_flag:
-            stream = io.BytesIO()
-            camera.capture(stream, format='jpeg', use_video_port=True)
-            yield stream.getvalue()
-            stream.seek(0)
-            stream.truncate()
+    with picamera.PiCamera() as camera:
+        camera.resolution = (300), (250)
+        camera.framerate = 20
+        time.sleep(2)
+        while True:
+            if generate_frames_flag:
+                stream = io.BytesIO()
+                camera.capture(stream, format='jpeg', use_video_port=True)
+                yield stream.getvalue()
+                stream.seek(0)
+                stream.truncate()
 
 
-@app.route('/home')
+@socketapp.route('/home')
 def index():
     return render_template('index_combined_sockets_test.html')
 
@@ -75,4 +61,4 @@ def handle_request_frame():
         print('Frames are already being generated. Ignoring request.')
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=8001, debug=False)
+    socketio.run(socketapp, host='0.0.0.0', port=8001, debug=False)
