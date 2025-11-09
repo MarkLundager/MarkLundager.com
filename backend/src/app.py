c084d1632eb237999e0189e6a5abcb9f93f99c35
Mark Lundage <mark_06_20@outlook.com>
2025-11-09 01:51:06 +0100

    ok

--- backend/src/app.py
+++ backend/src/app.py
@@ -17,40 +17,6 @@ app.register_blueprint(user_routes)
 CORS(app)
     
 
-@app.route('/timeUntilCanada', methods=['GET'])
-def timeUntilCanada():
-    return calculate_time_remaining()
-
-
-def calculate_time_remaining():
-    # Get the current date and time
-    current_datetime = datetime.now()
-
-    # Set the target date (June 6th)
-    target_date = datetime(current_datetime.year, 6, 6)
-
-    # Calculate the time difference
-    time_difference = target_date - current_datetime
-    total_seconds_remaining = int(time_difference.total_seconds())
-
-    # Extract days, hours, minutes, and seconds
-    days_remaining = time_difference.days
-    hours_remaining, remainder = divmod(time_difference.seconds, 3600)
-    minutes_remaining, seconds_remaining = divmod(remainder, 60)
-
-    # Display the time remaining
-    print(f"Time remaining until June 6th: {days_remaining} days, {hours_remaining} hours, {minutes_remaining} minutes, {seconds_remaining} seconds")
-    return jsonify({'days': days_remaining,
-                    'hours': hours_remaining,
-                    'minutes': minutes_remaining,
-                    'seconds': seconds_remaining,
-                    'totalSeconds': total_seconds_remaining
-                    })
-
-
-
-    # Implement a function to load the user from your database
-    # Example: return User(user_id, username, authority)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
